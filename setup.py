93ebb72ab99498d55f32301c32a0884071db33e0
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-02-19 13:58:33 -0500

    Add setup.py interface to build

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+# shim for compatibility with setuptools build workflow
+# adapted from https://stackoverflow.com/a/58754298
+
+from setuptools import setup
+
+setup()
