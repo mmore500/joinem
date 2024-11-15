6933b4a5ef3637c9fd5685eb2909d2b4780396aa
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-11-15 01:50:56 -0500

    Bump version: 0.8.1 → 0.8.2

--- joinem/__init__.py
+++ joinem/__init__.py
@@ -2,7 +2,7 @@ __author__ = "Matthew Andres Moreno"
 __copyright__ = "Copyright 2024, Matthew Andres Moreno"
 __credits__ = ["Matthew Andres Moreno"]
 __license__ = "MIT"
-__version__ = "0.8.1"
+__version__ = "0.8.2"
 __maintainer__ = "Matthew Andres Moreno"
 __email__ = "m.more500@gmail.com"
 
