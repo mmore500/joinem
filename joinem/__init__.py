5c9f6dcb88ef01f6758605e1dc04e21645422377
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2025-10-19 05:54:27 -0400

    Revert "Bump version: 0.10.0 → 0.11.0"
    This reverts commit f3d748654bdcb1a8d888b729bc871e56d6590c6b.

--- joinem/__init__.py
+++ joinem/__init__.py
@@ -2,7 +2,7 @@ __author__ = "Matthew Andres Moreno"
 __copyright__ = "Copyright 2024, Matthew Andres Moreno"
 __credits__ = ["Matthew Andres Moreno"]
 __license__ = "MIT"
-__version__ = "0.11.0"
+__version__ = "0.10.0"
 __maintainer__ = "Matthew Andres Moreno"
 __email__ = "m.more500@gmail.com"
 
